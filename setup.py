fdb49e485d86f457ed479eb3b0c0484241fefc0a
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-03-14 17:32:58 +0000

    implement setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import doctest
+import os
+import unittest
+
+from setuptools import find_packages, setup
+
+requirements = []
+
+with open("README.md", "r") as readme:
+    README = readme.read()
+
+exec(open("src/poaching_simulator/version.py", "r").read())
+
+setup(
+    name="blackbook",
+    version=__version__,
+    install_requires=requirements,
+    author="Nikoleta Glynatsi, Vince Knight",
+    author_email=("glynatsine@cardiff.ac.uk"),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="",
+    license="The MIT License (MIT)",
+    description="An agent based poaching simulation tool.",
+    long_description=README,
+)
