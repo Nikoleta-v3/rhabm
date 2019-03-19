79caedbba6b06bd7b0cdb7e06d51cd7348e733a8
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-03-19 17:18:49 +0000

    start working on agents

--- src/rhabm/agents.py
+++ src/rhabm/agents.py
@@ -5,6 +5,9 @@ from rhabm import UnoccupiedEmoji
 
 RhinoEmoji = "🦏"
 DeadRhinoEmoji = "💀"
+PoacherEmoji = "🔪"
+CaughtPoacherEmoji = "⛓️"
+SecurityEmoji = "🚓"
 
 
 def euclidean_distance(point_a, point_b):
@@ -34,7 +37,7 @@ class Rhino:
         True when the agent is mobile, False eitherwise. A rhino
         becomes immobile once they meet a poacher.
     dead : `bool`
-        True when the rhino is alive, False otherwise.
+        False when the rhino is alive, True otherwise.
     """
 
     def __init__(self, park, value=1):
@@ -74,121 +77,228 @@ class Rhino:
         return DeadRhinoEmoji
 
 
-# class Poacher(Rhino):
+class Poacher(Rhino):
+    """ A class to represent a poacher agent within the simulation.
 
-#     def __init__(self, park,
-#                  vision_radius=3,
-#                  movement_radius=1,
-#                  time_to_remove_rhino=4,
-#                  minimum_horn_value_threshold=0,
-#                  target_value=1):
+    Parameters
+    ==========
 
-#         self.vision_radius = vision_radius
-#         self.time_to_remove_rhino = time_to_remove_rhino
-#         self.movement_radius = movement_radius
-#         self.target_agent = None
-#         self.target_value = target_value
-#         self.minimum_horn_value_threshold = minimum_horn_value_threshold
-#         self.caught = False
-#         super().__init__(park)
+    park : `Park` instance
+        A park instance. The park which is being simulated.
+    vision_radius : `int`
+        The vision radius of a poacher.
+    movement_radius : `int`
+        The moving radius of a poacher.
+    time_to_remove_rhino : `int`
+        Time units the poacher needs to remove a horn.
+    minimum_horn_value_threshold : `float`
+        How selective is the poacher
+    target_value : `int`
+        The amount of rhino horn value the poacher needs to collect before
+        leaving the park.
 
-#     def find_individual(self, target="🦏"):
-#         target_cells_in_vision = [(i, j) for i, j in self.park.get_neighbours(*self.location, radius=self.vision_radius)
-#                                  if (self.park.occupants[i][j].__repr__() == target) and
-#                                     (self.park.occupants[i][j].value > self.minimum_horn_value_threshold)]
-#         if len(target_cells_in_vision) > 0:
-#             target_cells_in_vision.sort(key=lambda cell: euclidean_distance(cell, self.location))
-#             return target_cells_in_vision[0]
-#         return False
+    Attributes
+    ==========
+    location : `tuple`
+        A tuple of (i, j) coordinates. The current location of the poacher in the park.
+    is_mobile : `bool`
+        True when the agent is mobile, False eitherwise. A poacher
+        becomes immobile once they meet a rhino.
+    caught : `bool`
+        True when the poacher is caught by security, False otherwise.
+    """
 
-#     def move(self):
+    def __init__(
+        self,
+        park,
+        vision_radius=3,
+        movement_radius=1,
+        time_to_remove_rhino=4,
+        minimum_horn_value_threshold=0,
+        target_value=1,
+    ):
 
-#         if self.is_mobile:
-#             target_cell_in_vision = self.find_individual()
-#             neighbours = [(i, j) for i, j in self.park.get_neighbours(*self.location, radius=self.movement_radius)
-#                               if self.park.occupants[i][j] == UnoccupiedEmoji]
+        self.vision_radius = vision_radius
+        self.time_to_remove_rhino = time_to_remove_rhino
+        self.movement_radius = movement_radius
+        self.target_agent = None
+        self.target_value = target_value
+        self.minimum_horn_value_threshold = minimum_horn_value_threshold
+        self.caught = False
+        super().__init__(park)
 
-#             if target_cell_in_vision is not False:
+    def find_individual(self, target=RhinoEmoji):
+        """
+        A method for finding target.
 
-#                 if target_cell_in_vision in self.park.get_neighbours(*self.location):
-#                     target_agent = self.park.occupants[target_cell_in_vision[0]][target_cell_in_vision[1]]
-#                     if target_agent.is_mobile:
-#                         self.is_mobile = False
-#                         self.target_agent = target_agent
-#                         self.target_agent.is_mobile = False
-#                         return None
+        First, poachers check wheather there are rhinos within their radius of
+        vision.
 
-#                 best_next_distance = min([euclidean_distance(cell, target_cell_in_vision) for cell in neighbours])
-#                 potential_cells = [cell for cell in neighbours
-#                                    if euclidean_distance(cell, target_cell_in_vision) == best_next_distance]
+        If there are, they check wheather the rhino horn's value is bigger than
+        their threshold.
+
+        If there are more than a single rhino that satisfy these conditions,
+        then target the rhino which is closer to their current location.
+
+        Method returns `False` if no targets are within the radius of vision.
+        """
+        target_cells_in_vision = [
+            (i, j)
+            for i, j in self.park.get_neighbours(
+                *self.location, radius=self.vision_radius
+            )
+            if (self.park.occupants[i][j].__repr__() == target)
+            and (
+                self.park.occupants[i][j].value
+                > self.minimum_horn_value_threshold
+            )
+        ]
+        if len(target_cells_in_vision) > 0:
+            target_cells_in_vision.sort(
+                key=lambda cell: euclidean_distance(cell, self.location)
+            )
+            return target_cells_in_vision[0]
+        return False
+
+    def move(self):
+
+        if self.is_mobile:
+            target_cell_in_vision = self.find_individual()
+            neighbours = [
+                (i, j)
+                for i, j in self.park.get_neighbours(
+                    *self.location, radius=self.movement_radius
+                )
+                if self.park.occupants[i][j] == UnoccupiedEmoji
+            ]
+
+            if target_cell_in_vision is not False:
+
+                if target_cell_in_vision in self.park.get_neighbours(
+                    *self.location
+                ):
+                    target_agent = self.park.occupants[
+                        target_cell_in_vision[0]
+                    ][target_cell_in_vision[1]]
+                    if target_agent.is_mobile:
+                        self.is_mobile = False
+                        self.target_agent = target_agent
+                        self.target_agent.is_mobile = False
+                        return None
+
+                best_next_distance = min(
+                    [
+                        euclidean_distance(cell, target_cell_in_vision)
+                        for cell in neighbours
+                    ]
+                )
+                potential_cells = [
+                    cell
+                    for cell in neighbours
+                    if euclidean_distance(cell, target_cell_in_vision)
+                    == best_next_distance
+                ]
+
+            else:
+                potential_cells = neighbours
+
+            try:
+                new_location = random.choice(potential_cells)
+                self.park.occupants[self.location[0]][
+                    self.location[1]
+                ] = UnoccupiedEmoji
+                self.location = new_location
+                self.park.occupants[self.location[0]][self.location[1]] = self
+            except IndexError:
+                pass
+
+        else:
+            self.engage_target()
+
+    def engage_target(self):
+        if self.caught is False:
+            if self.time_to_remove_rhino > 0:
+                self.time_to_remove_rhino -= 1
+            elif not self.target_agent.dead:
+                self.target_agent.dead = True
+                self.target_value -= self.target_agent.value
+            elif self.target_value <= 0:
+                new_location = min(
+                    (
+                        self.location[0],
+                        (
+                            self.location[0] - self.movement_radius,
+                            self.location[1],
+                        ),
+                    ),
+                    (
+                        self.location[1],
+                        (
+                            self.location[0],
+                            self.location[1] - self.movement_radius,
+                        ),
+                    ),
+                    (
+                        self.park.width - self.location[0],
+                        (
+                            self.location[0] + self.movement_radius,
+                            self.location[1],
+                        ),
+                    ),
+                    (
+                        self.park.height - self.location[1],
+                        (
+                            self.location[0],
+                            self.location[1] + self.movement_radius,
+                        ),
+                    ),
+                    key=lambda pair: pair[0],
+                )[1]
+
+                self.park.occupants[self.location[0]][
+                    self.location[1]
+                ] = UnoccupiedEmoji
+                if (
+                    0 <= new_location[0] < self.park.width
+                    and 0 <= new_location[1] < self.park.height
+                ):
+                    self.location = new_location
+                    self.park.occupants[self.location[0]][
+                        self.location[1]
+                    ] = self
+            else:
+                self.is_mobile = True
+
+    def __repr__(self):
+        if self.caught:
+            return CaughtPoacherEmoji
+        return PoacherEmoji
 
 
-#             else:
-#                 potential_cells = neighbours
+class SecurityOfficer(Poacher):
 
-#             try:
-#                 new_location = random.choice(potential_cells)
-#                 self.park.occupants[self.location[0]][self.location[1]] = UnoccupiedEmoji
-#                 self.location = new_location
-#                 self.park.occupants[self.location[0]][self.location[1]] = self
-#             except IndexError:
-#                 pass
+    def __init__(self, park,
+                 vision_radius=3,
+                 movement_radius=1,
+                ):
 
-#         else:
-#             self.engage_target()
+        self.vision_radius = vision_radius
+        self.movement_radius = movement_radius
+        self.target_agent = None
+        super().__init__(park)
 
-#     def engage_target(self):
-#         if self.caught is False:
-#             if self.time_to_remove_rhino > 0:
-#                 self.time_to_remove_rhino -= 1
-#             elif not self.target_agent.dead:
-#                 self.target_agent.dead = True
-#                 self.target_value -= self.target_agent.value
-#             elif self.target_value <= 0:
-#                 new_location = min((self.location[0], (self.location[0] - self.movement_radius, self.location[1])),
-#                                    (self.location[1], (self.location[0], self.location[1] - self.movement_radius)),
-#                                    (self.park.width - self.location[0], (self.location[0] + self.movement_radius, self.location[1])),
-#                                    (self.park.height - self.location[1], (self.location[0], self.location[1] + self.movement_radius)),
-#                                    key=lambda pair:pair[0])[1]
+    def find_individual(self, target=PoacherEmoji):
+        return super().find_individual(target=target)
 
-#                 self.park.occupants[self.location[0]][self.location[1]] = UnoccupiedEmoji
-#                 if 0 <= new_location[0] < self.park.width and 0 <= new_location[1] < self.park.height:
-#                     self.location = new_location
-#                     self.park.occupants[self.location[0]][self.location[1]] = self
-#             else:
-#                 self.is_mobile = True
+    def engage_target(self):
+        self.target_agent.caught = True
 
+        try:
+            self.target_agent.target_agent.is_mobile = True
+        except AttributeError:
+            pass
 
-#     def __repr__(self):
-#         if self.caught:
-#             return "⛓️"
-#         return "🔪"
+        self.is_mobile = True
 
-
-# class SecurityOfficer(Poacher):
-
-#     def __init__(self, park,
-#                  vision_radius=3,
-#                  movement_radius=1,
-#                 ):
-
-#         self.vision_radius = vision_radius
-#         self.movement_radius = movement_radius
-#         self.target_agent = None
-#         super().__init__(park)
-
-#     def find_individual(self, target="🔪"):
-#         return super().find_individual(target=target)
-
-#     def engage_target(self):
-#         self.target_agent.caught = True
-
-#         try:
-#             self.target_agent.target_agent.is_mobile = True
-#         except AttributeError:
-#             pass
-
-#         self.is_mobile = True
-
-#     def __repr__(self):
-#         return "🚓"
+    def __repr__(self):
+        return SecurityEmoji
